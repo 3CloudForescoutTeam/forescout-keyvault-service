3391720c0cecedfee73b05ca00c01383418b17f0
Daniel Acquah <daniel.acquah@3Cloud-TXD6XHC72K.fios-router.home>
2024-04-12 11:00:35 -0400

    initial push

---
+++ src/main/java/com/forescout/cysiv/keyvault/KeyVaultService.java
@@ -0,0 +1,12 @@
+package com.forescout.cysiv.keyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyVaultService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyVaultService.class, args);
+    }
+}
