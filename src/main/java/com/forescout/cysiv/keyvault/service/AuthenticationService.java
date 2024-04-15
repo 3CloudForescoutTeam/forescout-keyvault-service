f1da0d211a3f6bc27b92e8a74691906fd71f7bd3
Daniel Acquah <daniel.acquah@3cloudsolutions.com>
2024-04-15 13:24:32 -0400

    fixes

---
+++ src/main/java/com/forescout/cysiv/keyvault/service/AuthenticationService.java
@@ -0,0 +1,33 @@
+package com.forescout.cysiv.keyvault.service;
+
+import com.forescout.cysiv.keyvault.model.JwtTokenDTO;
+import com.forescout.cysiv.keyvault.model.LoginDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public JwtTokenDTO login(LoginDTO loginDTO) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
+
+        if (!authentication.isAuthenticated())
+            throw new UsernameNotFoundException("Invalid Credentials. Username Not Found");
+
+        return JwtTokenDTO.builder()
+                .accessToken(jwtService.generateToken(loginDTO.getUsername()))
+                .build();
+    }
+}
